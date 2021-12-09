c6c090ef2ede50caed1663a7ff42c5839ecf7a9b
Kenneth Koski <kennethkoski@gmail.com>
2021-12-08 22:23:01 -0600

    2021 Day 08 - bitfields yay

--- 2021/day08.py
+++ 2021/day08.py
@@ -1,13 +1,25 @@
+# Convert "words" to bitfields where 'a' is LSB and 'g' is MSB. For example,
+# convert `ef` to `0b110000`. It's similar to doing `set('ef')`, but both more
+# efficient, and easier to write code for with bitwise operators.
 with open("day08.txt") as f:
-    lines = [[item.strip().split(" ") for item in line.strip().split("|")] for line in f.readlines()]
+    lines = [
+        [
+            [
+                sum(1 << (ord(ch) - 97) for ch in word)
+                for word in side.strip().split(" ")
+            ]
+            for side in line.strip().split("|")
+        ]
+        for line in f.readlines()
+    ]
 
-total = sum(len(digit) in (2, 4, 3, 7) for _, digits in lines for digit in digits)
+total = sum(bin(digit).count("1") in (2, 4, 3, 7) for _, digits in lines for digit in digits)
 
 print(total)
 
 
 def get(pats, length, filt=lambda _: True):
-    return "".join(sorted(next(i for i in pats if len(i) == length and filt(i))))
+    return next(i for i in pats if bin(i).count("1") == length and filt(i))
 
 
 total = 0
@@ -19,14 +31,14 @@ for patterns, digits in lines:
         8: get(patterns, 7),
     }
 
-    solved[3] = get(patterns, 5, lambda i: all(x in i for x in solved[1]))
-    solved[5] = get(patterns, 5, lambda i: not all(x in i for x in solved[1]) and sum([x in i for x in solved[4]]) == 3)
-    solved[2] = get(patterns, 5, lambda i: not all(x in i for x in solved[1]) and sum([x in i for x in solved[4]]) == 2)
-    solved[0] = get(patterns, 6, lambda i: all(x in i for x in solved[7]) and not all(x in i for x in solved[4]))
-    solved[6] = get(patterns, 6, lambda i: not all(x in i for x in solved[7]))
-    solved[9] = get(patterns, 6, lambda i: all(x in i for x in solved[4]))
+    solved[3] = get(patterns, 5, lambda i: i & solved[1] == solved[1])
+    solved[2] = get(patterns, 5, lambda i: i & solved[1] != solved[1] and bin(i & solved[4]).count("1") == 2)
+    solved[5] = get(patterns, 5, lambda i: i & solved[1] != solved[1] and bin(i & solved[4]).count("1") == 3)
+    solved[6] = get(patterns, 6, lambda i: i & solved[7] != solved[7])
+    solved[9] = get(patterns, 6, lambda i: i & solved[4] == solved[4])
+    solved[0] = get(patterns, 6, lambda i: i & solved[7] == solved[7] and i & solved[4] != solved[4])
 
     decoder = {v: k for k, v in solved.items()}
-    total += int("".join(str(decoder["".join(sorted(x))]) for x in digits))
+    total += int("".join(str(decoder[x]) for x in digits))
 
 print(total)
